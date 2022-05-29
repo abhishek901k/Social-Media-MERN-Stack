cd4378cd11dedf00e78f78f386ef9dcf13502c5e
unknown <abhishek901k.com>
2022-05-29 17:11:08 +0530

    created basic setup for PostSide

---
+++ src/components/PostSide/PostSide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './PostSide'
+
+const PostSide = () => {
+    return (
+        <div className="PostSide">
+            SharedComponent
+        </div>
+    )
+}
+
+export default PostSide
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PostSide from '../components/PostSide/PostSide'
 import ProfileSide from '../components/ProfileSide/ProfileSide'
 import './Home.css'
 
@@ -7,7 +8,7 @@ const Home = () => {
     return (
         <div className='Home'>
             <ProfileSide/>
-            <div className="postSide">Posts</div>
+            <PostSide/>
             <div className="RightSide">RightSide</div>
         </div>
     )
