0f306d58c3a20486dc1b75543fb1f45d3911fc29
unknown <abhishek901k.com>
2022-05-29 12:25:48 +0530

    FollowersCArd : read data from followers data & displayed it

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import './FollowersCard.css';
 
-import './FollowersCard.css'
+import { Followers } from "../../Data/FollowersData";
 
 const FollowersCard = () => {
     return (
         <div className="FollowersCard">
-            FollowersCard
+            <h3> Who is following you</h3>
+
+            {Followers.map((Follower, id) => {
+                return (
+                    <div className="Follower">Yes Follower</div>
+                )
+            })}
         </div>
     )
 }
