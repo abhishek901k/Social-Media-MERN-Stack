c31d6e5d6e61fb9607b4305603f5e10636d77f6f
unknown <abhishek901k.com>
2022-05-29 16:58:28 +0530

    created Followers card for the Profile Side

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,20 +1,30 @@
-import React from "react";
-import './FollowersCard.css';
+import React from 'react'
+import './FollowersCard.css'
 
-import { Followers } from "../../Data/FollowersData";
-
+import { Followers } from '../../Data/FollowersData'
 const FollowersCard = () => {
-    return (
-        <div className="FollowersCard">
-            <h3> Who is following you</h3>
+  return (
+    <div className="FollowersCard">
+        <h3>Who is following you</h3>
 
-            {Followers.map((Follower, id) => {
-                return (
-                    <div className="Follower">Yes Follower</div>
-                )
-            })}
-        </div>
-    )
+        {Followers. map((follower, id)=>{
+            return(
+                <div className="follower">
+                    <div>
+                        <img src={follower.img} alt="" className='followerImage' />
+                        <div className="name">
+                            <span>{follower.name}</span>
+                            <span>@{follower.username}</span>
+                        </div>
+                    </div>
+                    <button className='button fc-button'>
+                        Follow
+                    </button>
+                </div>
+            )
+        })}
+    </div>
+  )
 }
 
 export default FollowersCard
