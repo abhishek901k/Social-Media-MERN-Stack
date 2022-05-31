b74c901044840f9151bacc80973e65faec8408b6
unknown <abhishek901k.com>
2022-05-31 12:16:42 +0530

    created left side of the auth page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './Auth.css'
+import Logo from '../../img/logo.png'
 
 const Auth = () => {
     return (
         <div className="Auth">
-            Auth
+            <div className="a-left">
+                <img src={Logo} alt="" />
+                <div className="Webname">
+                    <h1>Abhi Media</h1>
+                    <h6>My idea My idea My ideaMy ideaMy idea</h6>
+                </div>
+            </div>
         </div>
     )
 }
